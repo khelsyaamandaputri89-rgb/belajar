7b3b6c44ffdce56d0fe479f123e329f4eddbff6f
khelsyaamandaputri89-rgb <khelsyaamandaputri89@gmail.com>
2025-08-14 10:45:28 +0700

    menambahkan materi minggu ke 1 dan 2

---
+++ operator js/script.js
@@ -0,0 +1,25 @@
+//tambah
+const a = 10
+const b = 15
+const tambah = a + b
+console.log(tambah)
+//kurang
+const c = 100
+const d = 50
+const kurang = c - d
+console.log(kurang)
+//kali
+const e = 5
+const f = 50
+const kali = e * f
+console.log(kali)
+//bagi
+const g = 500
+const h = 50
+const bagi = g / h
+console.log(bagi)
+//hasil bagi
+const i = 100
+const j = 33
+const hasilBagi = i % j
+console.log(hasilBagi)
---
+++ tipe data js/script.js
@@ -0,0 +1,26 @@
+//string
+const nama = "monika"
+console.log(nama)
+//number
+const number = "1"
+console.log(number)
+//boolean
+let benar = true
+let salah = false
+console.log(benar)
+console.log(salah)
+//null
+let age = null;
+console.log(age)
+//undefened
+let name;
+console.log(name)
+//array
+let hewan = ["kucing", "bebek", "kuda"]
+console.log(hewan)
+//object
+var user = {
+    nama : "khelsya",
+    umur : 17
+}
+console.log(user.nama)
